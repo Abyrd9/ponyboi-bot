9f6d3ca347346717dc691df3298b901bab6ef91b
Abyrd9 <andrew.byrd99@gmail.com>
2020-02-21 14:30:40 -0500

    making fixes to fields block so if we have more than 10 it won't break

--- src/ponyActions/startPonyGame.js
+++ src/ponyActions/startPonyGame.js
@@ -5,28 +5,31 @@ exports.startPonyGame = (ponybot, channel, messageId, users) => {
   let fields = [];
   if (users.length > 0) shuffled = shuffle(users);
   if (shuffled.length > 0) {
-    fields = [
-      {
-        "type": "mrkdwn",
-        "text": "*Name*"
-      },
-      {
-        "type": "mrkdwn",
-        "text": "*Order*"
-      },
-    ];
     shuffled.forEach((value, index) => {
       let number = index + 1;
       fields.push({
-        "type": "mrkdwn",
-        "text": "*" + value + "*"
-      });
-      fields.push({
-        "type": "mrkdwn",
-        "text": "*" + number + "*"
+        "type": "section",
+        "text": {
+          "type": "mrkdwn",
+          "text": "*" + number + "*" +  "  -  " + value,
+        }
       });
     })
   }
+
+  let blocks = [
+    {
+      type: "section",
+      text: {
+        text: "*The Pony Order:*",
+        type: "mrkdwn"
+      }
+    },
+    {
+      type: "divider"
+    },
+    ...fields,
+  ];
 
   return new Promise((resolve, reject) => {
     resolve(ponybot.chat.update({
@@ -35,16 +38,7 @@ exports.startPonyGame = (ponybot, channel, messageId, users) => {
       ts: messageId,
       as_user: true,
       link_names: true,
-      blocks: [
-        {
-          "type": "section",
-          "text": {
-            "text": "*The Pony Order:*",
-            "type": "mrkdwn"
-          },
-          "fields": fields
-        }
-      ]
+      blocks: JSON.stringify(blocks)
     }))
   })
 } 
